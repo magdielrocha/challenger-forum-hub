9f079dab6d6249ab3d013534177b9f64a33ce205
magdielrocha <magdiel002@gmail.com>
2025-08-19 17:00:12 -0300

    adiciona configurações do application.properties

---
+++ src/main/java/br/com/alura/challenger_forum_hub/ChallengerForumHubApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.challenger_forum_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerForumHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengerForumHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/challenger_forum_hub/ChallengerForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.challenger_forum_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengerForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
